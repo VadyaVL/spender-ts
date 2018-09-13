34e81fe6cec6bf4f1e3208f85235e711f339bd01
verezh <verezh@gmail.com>
2018-09-13 18:13:53 +0300

    sts

---
+++ 1.js
@@ -0,0 +1,41 @@
+var dataTree = [
+          {
+            text: "Parent 1",
+            nodes: [
+              {
+                text: "Child 1",
+                nodes: [
+                  {
+                    text: "Grandchild 1"
+                  },
+                  {
+                    text: "Grandchild 2"
+                  }
+                ]
+              },
+              {
+                text: "Child 2"
+              }
+            ]
+          },
+          {
+            text: "Parent 2"
+          },
+          {
+            text: "Parent 3"
+          },
+          {
+            text: "Parent 4"
+          },
+          {
+            text: "Parent 5"
+          }
+        ];
+         
+        var treeView = $('#tree-view', currentTask);
+        
+        console.log(treeView);
+        console.log(treeView.treeview);
+        if (treeView != null && typeof treeView.treeview == 'function') {
+          treeView.treeview({data: dataTree});
+        }
